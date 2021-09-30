1e293eb608946c4ab445906449d603f56e5b5fcf
MarekHlavka <johnsonmarek@gmail.com>
2021-09-30 14:12:27 +0200

    idk

--- secret.c
+++ secret.c
@@ -5,10 +5,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/sockets.h>
+
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
 #define CLIENT_MODE = 0
 #define SERVER_MODE = 1
 
-int main(int argc, char *argv[]){
+#define DEF_PORT = 9245
 
+int main(int argc, char *argv[]) {
+
+    if (argv[1] == CLIENT_MODE) {}    // create a socket
+        int my_socket;
+        my_socket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
+        // adresa socketu
+        struct sockaddr_in server_address;
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons( DEF_PORT);
+        server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        int connection_status = connect(my_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+        if (connection_status == -1) {
+            printf("Error making connection");
+        }
+
+        // prijimat data
+        char server_response[256];
+        recv(my_socket, &server_response, sizeof(server_response), 0);
+
+        print("Response: %s", server_response);
+
+        close(my_socket);
+    }
+    else{
+
+    }
+
+    return 0;
 }
 
