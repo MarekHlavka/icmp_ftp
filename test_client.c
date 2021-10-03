4d8c78610e174404493bd766b8c8ebaea1392d8b
MarekHlavka <johnsonmarek@gmail.com>
2021-10-03 20:00:52 +0200

    server

--- test_client.c
+++ test_client.c
@@ -3,26 +3,46 @@
 #include <stdio.h>
 
 int main(){
-	struct icmp_packet packet;
-	char *src_ip;
-	char *dst_ip;
-	int socket_id;
 
-	src_ip = "127.0.0.2";
-	dst_ip = "127.0.0.1";
+	if(1){
+		struct icmp_packet packet;
+		char *src_ip;
+		char *dst_ip;
+		int socket_id;
 
-	strncpy(packet.src_addr, src_ip, strlen(src_ip) + 1);
-	strncpy(packet.dest_addr, dst_ip, strlen(src_ip) + 1);
+		src_ip = "127.0.0.2";
+		dst_ip = "127.0.0.1";
 
-	set_reply_type(&packet);
-	packet.payload = "Hello there!";
-	packet.payload_size = strlen(packet.payload);
+		strncpy(packet.src_addr, src_ip, strlen(src_ip) + 1);
+		strncpy(packet.dest_addr, dst_ip, strlen(src_ip) + 1);
 
-	socket_id = open_icmp_socket();
-	printf("%d\n", socket_id);
+		set_reply_type(&packet);
+		packet.payload = "Hello there!";
+		packet.payload_size = strlen(packet.payload);
 
-	printf("Sending...\n");
-	send_icmp_packet(socket_id, &packet);
-	printf("Closing...\n");
-	close_icmp_socket(socket_id);
+		socket_id = open_icmp_socket();
+		printf("%d\n", socket_id);
+
+		printf("Sending...\n");
+		send_icmp_packet(socket_id, &packet);
+		printf("Closing...\n");
+		close_icmp_socket(socket_id);
+	}
+	else{
+		struct icmp_packet packet;
+		int socket_id;
+
+		socket_id = open_icmp_socket();
+		bind_icmp_socket(socket_id);
+
+		printf("Server initialized...\n");
+		while(1){
+			recieve_icmp_packet(socket_id, &packet);
+			printf("%s\n", packet.src_addr);
+			printf("%s\n", packet.dest_addr);
+			printf("%s\n", packet.type);
+			printf("%s\n", packet.payload);
+		}
+		close_icmp_socket(socket_id);
+	}
 }
