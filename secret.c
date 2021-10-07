ae5a277efe53f39e14fe434b1427f45fb4182c5e
MarekHlavka <johnsonmarek@gmail.com>
2021-10-07 18:46:58 +0200

    adad

--- secret.c
+++ secret.c
@@ -2,8 +2,24 @@
 #include "server.h"
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
+/* Usage: 
+	secret -r <file> -s <ip|hostname> [-l]
+		-r <file> : specifikace souboru pro přenos
+		-s <ip|hostname> : ip adresa/hostname na kterou se má soubor zaslat
+		-l : pokud je program spuštěn s tímto parametrem, jedná se o server,
+			který naslouchá příchozím ICMP zprávám a ukládá soubor do stejného adresáře,
+			kde byl spuštěn.
+
+*/
 int main(int argc, char** argv){
+
+	int argument;
+
+	while((argument = getopt(argc, argv, "r:s:l")) != -1){
+		break;
+	}
 
 	if(argc == 2){
 		run_client();
